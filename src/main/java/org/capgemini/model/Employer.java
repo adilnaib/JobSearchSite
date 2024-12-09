02b8f5ddd1a8c861f300cfe76838d3386e049df6
cganiket <aniket.choudhary@capgemini.com>
2024-12-09 12:38:07 +0530

    Some pojo's and service skeletons

---
+++ src/main/java/org/capgemini/model/Employer.java
@@ -0,0 +1,60 @@
+package org.capgemini.model;
+
+import sun.jvm.hotspot.ui.tree.CStringTreeNodeAdapter;
+
+public class Employer {
+    private String empOrg;
+    private String empAddress;
+    private double empContact;
+    private String empEmail;
+    private String username;
+    private String empname;
+
+    public String getEmpOrg() {
+        return empOrg;
+    }
+
+    public String getEmpAddress() {
+        return empAddress;
+    }
+
+    public void setEmpAddress(String empAddress) {
+        this.empAddress = empAddress;
+    }
+
+    public double getEmpContact() {
+        return empContact;
+    }
+
+    public void setEmpContact(double empContact) {
+        this.empContact = empContact;
+    }
+
+    public String getEmpEmail() {
+        return empEmail;
+    }
+
+    public void setEmpEmail(String empEmail) {
+        this.empEmail = empEmail;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmpname() {
+        return empname;
+    }
+
+    public void setEmpname(String empname) {
+        this.empname = empname;
+    }
+
+    public void setEmpOrg(String empOrg) {
+        this.empOrg = empOrg;
+    }
+}
---
+++ src/main/java/org/capgemini/services/EmployerService.java
@@ -0,0 +1,13 @@
+package org.capgemini.services;
+
+public class EmployerService {
+
+    public String jobPosting(){
+        String output = "";
+        return output;
+    }
+
+    public void jobEdit(){
+
+    }
+}
