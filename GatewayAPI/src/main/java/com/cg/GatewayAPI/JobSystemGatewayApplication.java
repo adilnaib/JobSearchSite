7ddc036a6c1654dd4c0afac94a3e8c3c1d8d6a3d
adilnaib <naib-mohammed.adil@capgemini.com>
2024-12-18 11:27:21 +0530

    Gateway update

--- GatewayAPI/src/main/java/com/cg/GatewayAPI/JobSystemGatewayApplication.java
+++ GatewayAPI/src/main/java/com/cg/GatewayAPI/JobSystemGatewayApplication.java
@@ -3,10 +3,29 @@ package com.cg.GatewayAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class JobSystemGatewayApplication {
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(List.of("*"));
+		config.addAllowedHeader("*");
+		config.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return new CorsWebFilter(source);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobSystemGatewayApplication.class, args);
