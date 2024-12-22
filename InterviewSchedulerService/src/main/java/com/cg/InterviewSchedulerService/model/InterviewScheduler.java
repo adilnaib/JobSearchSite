ded3ca38c9b85a0c42ea0fa819716082d2e45e69
adilnaib <naib-mohammed.adil@capgemini.com>
2024-12-22 22:02:18 +0530

    .

--- InterviewSchedulerService/src/main/java/com/cg/InterviewSchedulerService/model/InterviewScheduler.java
+++ InterviewSchedulerService/src/main/java/com/cg/InterviewSchedulerService/model/InterviewScheduler.java
@@ -2,17 +2,12 @@ package com.cg.InterviewSchedulerService.model;
 
 import com.cg.sharedmodule.model.JobApplication;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
 public class InterviewScheduler {
 
     @Id
@@ -48,6 +43,158 @@ public class InterviewScheduler {
     private String jobTitle;
     private String jobLocation;
     private double jobSalary;
+
+    public Long getInterviewId() {
+        return interviewId;
+    }
+
+    public void setInterviewId(Long interviewId) {
+        this.interviewId = interviewId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMeetingLink() {
+        return meetingLink;
+    }
+
+    public void setMeetingLink(String meetingLink) {
+        this.meetingLink = meetingLink;
+    }
+
+    public String getPanelMembers() {
+        return panelMembers;
+    }
+
+    public void setPanelMembers(String panelMembers) {
+        this.panelMembers = panelMembers;
+    }
+
+    public String getInterviewMode() {
+        return interviewMode;
+    }
+
+    public void setInterviewMode(String interviewMode) {
+        this.interviewMode = interviewMode;
+    }
+
+    public String getInterviewType() {
+        return interviewType;
+    }
+
+    public void setInterviewType(String interviewType) {
+        this.interviewType = interviewType;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public JobApplication getJobApplication() {
+        return jobApplication;
+    }
+
+    public void setJobApplication(JobApplication jobApplication) {
+        this.jobApplication = jobApplication;
+    }
+
+    public String getSeekerName() {
+        return seekerName;
+    }
+
+    public void setSeekerName(String seekerName) {
+        this.seekerName = seekerName;
+    }
+
+    public String getSeekerEmail() {
+        return seekerEmail;
+    }
+
+    public void setSeekerEmail(String seekerEmail) {
+        this.seekerEmail = seekerEmail;
+    }
+
+    public String getSeekerContact() {
+        return seekerContact;
+    }
+
+    public void setSeekerContact(String seekerContact) {
+        this.seekerContact = seekerContact;
+    }
+
+    public String getEmployerName() {
+        return employerName;
+    }
+
+    public void setEmployerName(String employerName) {
+        this.employerName = employerName;
+    }
+
+    public String getEmployerContact() {
+        return employerContact;
+    }
+
+    public void setEmployerContact(String employerContact) {
+        this.employerContact = employerContact;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getJobLocation() {
+        return jobLocation;
+    }
+
+    public void setJobLocation(String jobLocation) {
+        this.jobLocation = jobLocation;
+    }
+
+    public double getJobSalary() {
+        return jobSalary;
+    }
+
+    public void setJobSalary(double jobSalary) {
+        this.jobSalary = jobSalary;
+    }
 
     /**
      * Sets job-related details from the associated job application.
